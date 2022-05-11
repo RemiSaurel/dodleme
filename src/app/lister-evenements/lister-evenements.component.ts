30fe3adbbc442db063bce7744fc9e68322c9ef2a
RémiSaurel <r.saurel5@gmail.com>
2022-05-11 16:57:47 +0200

    premiers routages

---
+++ src/app/lister-evenements/lister-evenements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lister-evenements',
+  templateUrl: './lister-evenements.component.html',
+  styleUrls: ['./lister-evenements.component.css']
+})
+export class ListerEvenementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
