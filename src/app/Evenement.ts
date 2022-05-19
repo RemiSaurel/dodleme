7925f834c5371c03c3ca719bc226eed94de23090
RemiSaurel <r.saurel5@gmail.com>
2022-05-20 00:57:12 +0200

    Style creer event

--- src/app/Evenement.ts
+++ src/app/Evenement.ts
@@ -1,5 +1,5 @@
-// TODO : completer plus tard, pour tester on garde ça
 export class Evenement {
   titre: string;
   description: string;
+
 }
--- src/app/creer-evenement/creer-evenement.component.ts
+++ src/app/creer-evenement/creer-evenement.component.ts
@@ -18,8 +18,26 @@ export class CreerEvenementComponent implements OnInit {
     this.evenement = new Evenement();
   }
 
+  eventOk(): boolean {
+    return  this.evenement.titre !== undefined
+      && this.evenement.titre !== "";
+  }
+
+  clearInputs() {
+    this.evenement.titre = ""
+    this.evenement.description = ""
+  }
+
   valider(){
-    this.apiDodleMe.ajouterEvent(this.evenement)
+    document.getElementById("event_ok")?.classList.add("d-none");
+    if (this.eventOk()) {
+      this.apiDodleMe.ajouterEvent(this.evenement)
+      this.clearInputs();
+      document.getElementById("error_titre")?.classList.add("d-none");
+      document.getElementById("event_ok")?.classList.remove("d-none");
+    } else {
+      document.getElementById("error_titre")?.classList.remove("d-none")
+    }
   }
 
 }
