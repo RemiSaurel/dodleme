1eb23ef1d0e6caec69f84e80ff5b8a879d9d0c5b
RemiSaurel <r.saurel5@gmail.com>
2022-05-22 22:36:32 +0200

    fix

--- src/app/Creneau.ts
+++ src/app/Creneau.ts
@@ -1,10 +1,10 @@
 export class Creneau {
 
-  public static readonly NB_PROPRIETES_CRENEAU = 5;
+  public static readonly NB_PROPRIETES_CRENEAU = 4;
 
   date_debut: string
   date_fin: string
   heure_debut: string
   heure_fin: string
-  participants: [string]
+  //participants: [string] TODO : mettre participants
 }
